18bd6536fed6459c68c8c893732abca0b55caa6d
limseonghyun <sksrpf1126@naver.com>
2021-10-16 23:25:40 +0900

    first day

---
+++ project/app.js
@@ -0,0 +1,31 @@
+// 주석처리가 된 것들은 부족한 부분, 새로운 방법에 대한 것, 보충 설명임
+
+//로그인 폼의 자식들을 찾는 방법 1
+// const loginForm = document.querySelector('#login-form')
+// const loginInput = loginForm.querySelector('input')
+// const loginButton = loginForm.querySelector('button')
+
+const loginForm = document.querySelector('#login-form')
+//로그인 폼의 자식들을 찾는 방법 2 (더 간단)
+const loginInput = document.querySelector('#login-form input')
+//const loginButton = document.querySelector('#login-form button') form태그를 이용하면서 필요 없어짐
+
+//loginButton.addEventListener('click', onLoginBtnClick)
+function onLoginBtnClick() {
+  const userName = loginInput.value
+  // 아래의 방법으로도 유효성 검사를 실시할 수 있지만, html input 속성 중 required(필수 입력) maxlength(글자 제한)으로 검사 가능.
+  // 단, input의 위 속성들을 쓰기 위해서는 form 태그안에 존재해야함 하지만 form태그에서 버튼을 누르거나 type이 submit인 input을 누르면 제출되면서 새로고침이 일어남.
+  // if (userName === '') {
+  //   alert('이름을 입력해 주세요.')
+  // } else if (userName.length > 15) {
+  //   alert('이름이 너무 깁니다.')
+  // }
+}
+
+//엔터를 누르거나 submit속성의 input태그나, button을 눌를 때 동작
+function onLoginSubmit(event) {
+  event.preventDefault() //기본동작 제거(submit 이후 새로고침 되는 동작 포함)
+  const userName = loginInput.value
+}
+
+loginForm.addEventListener('submit', onLoginSubmit)
